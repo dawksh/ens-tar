917c25a1ab7640c2eb6d20b9dc9e14f444fd8c32
Daksh Kulshrestha <20741933+dawksh@users.noreply.github.com>
2022-10-27 00:35:43 +0530

    fix: fix alchemy provider import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ import {
 	midnightTheme,
 } from "@rainbow-me/rainbowkit";
 import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
-import { alchemyProvider } from "wagmi/providers/public";
+import { alchemyProvider } from "wagmi/providers/alchemy";
 import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
 import "@fontsource/inter/800.css";
 import "@fontsource/inter/400.css";
